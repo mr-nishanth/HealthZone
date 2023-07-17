87bce0e362ffcfe67d21b1b6794fd3d8291f22d8
Nishanth M <innocentnishanth@gmail.com>
2023-07-17 11:47:10 +0530

    [A] Basic Route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Hero } from './pages/Hero';
+import { SignUp } from './pages/SignUp';
+import { LogIn } from './pages/LogIn';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+export default function App() {
     return (
         <>
-            <h1>Health Zone</h1>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Hero />}
+                    />
+                    <Route
+                        path='/sign-up'
+                        element={<SignUp />}
+                    />
+                    <Route
+                        path='/login'
+                        element={<LogIn />}
+                    />
+                    <Route
+                        path='/dashboard'
+                        element={<Dashboard />}
+                    />
+                    <Route
+                        path='/profile'
+                        element={<Profile />}
+                    />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
         </>
     );
 }
-
-export default App;

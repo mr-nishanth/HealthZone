81fcc963667a0460ca2369929a82ccf9ba690be5
Nishanth M <innocentnishanth@gmail.com>
2023-07-17 11:45:53 +0530

    [A] Dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { SideBar } from '../components/SideBar';
+
+const Dashboard = () => {
+    return (
+        <div className='h-[79vh] w-full bg-gray-400'>
+            <SideBar />
+        </div>
+    );
+};
+
+export default Dashboard;

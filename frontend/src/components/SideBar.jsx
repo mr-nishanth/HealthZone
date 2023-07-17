01cb4904a722201b15419c884f94cee2c7a6b210
Nishanth M <innocentnishanth@gmail.com>
2023-07-17 11:45:32 +0530

    [A] SideBar

---
+++ frontend/src/components/SideBar.jsx
@@ -0,0 +1,49 @@
+import { BarChart, PersonStandingIcon } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+export function SideBar() {
+    return (
+        <aside className='flex h-[80vh] w-64 flex-col overflow-y-auto border-r bg-white px-5 py-8'>
+            <div className='mt-6 flex flex-1 flex-col justify-between'>
+                <nav className='-mx-3 space-y-6 '>
+                    <div className='space-y-3 '>
+                        <label className='px-3 text-xs font-semibold uppercase text-gray-900'>
+                            Settings
+                        </label>
+                        <Link
+                            className='flex transform items-center rounded-lg px-3 py-2 text-gray-600 transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700'
+                            to='/dashboard'
+                        >
+                            <BarChart
+                                className='h-5 w-5'
+                                aria-hidden='true'
+                            />
+                            <span className='mx-2 text-sm font-medium'>
+                                Dashboard
+                            </span>
+                        </Link>
+
+                        <Link
+                            to={'/profile'}
+                            className='flex transform items-center rounded-lg px-3 py-2 text-gray-600 transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700'
+                        >
+                            <PersonStandingIcon
+                                className='h-5 w-5'
+                                aria-hidden='true'
+                            />
+                            <span className='mx-2 text-sm font-medium'>
+                                Profile
+                            </span>
+                        </Link>
+                    </div>
+                </nav>
+                <div className='mt-6'>
+                    <span className='text-sm font-medium text-gray-700'>
+                        Username : Nishanth <br />
+                        Email : nishanthtemp@gmail.com
+                    </span>
+                </div>
+            </div>
+        </aside>
+    );
+}

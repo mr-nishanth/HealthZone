f61e392a5e19ee0c0a44eee27f8d5935d30b661e
Nishanth M <innocentnishanth@gmail.com>
2023-07-23 12:49:27 +0530

    Update corsOptions

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // CORS middleware
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 // Logger middleware

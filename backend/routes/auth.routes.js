810408d9bf010b4d8050e6422d6e02281f099d4b
Nishanth M <innocentnishanth@gmail.com>
2023-07-23 14:32:27 +0530

    Added Rate Limit Feature

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
+const rateLimit = require('express-rate-limit');
+
 const {
     registerUser,
     loginUser,
     logoutUser,
 } = require('../controllers/auth.controllers');
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 min
+    max: 2, // 2 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.',
+});
+
 // AUTH ROUTES
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
+router.route('/register').post([limiter, registerUser]);
+router.route('/login').post([limiter, loginUser]);
 router.route('/logout').get(logoutUser);
 
 module.exports = router;
